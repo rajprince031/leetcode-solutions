8a9c672f178ea5a0f5f41635f9f4a5bafd96f859
Prince Raj <rajprince031@gmail.com>
2026-08-06 01:33:27 +0530

    Time: 42 ms (86.34%), Space: 50.7 MB (5.92%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,28 +1,29 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int subWithMaxK = subarraysWithAtMostK(nums, k);
-        int reducedSubWithMaxK = subarraysWithAtMostK(nums, k - 1);
-        return subWithMaxK - reducedSubWithMaxK;
-    }
-
-    public int subarraysWithAtMostK(int[] nums, int k) 
-    {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int left = 0, right = 0, ans = 0;
-        while(right < nums.length)
-        {
-            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
-            while(map.size() > k)
-            {
-                map.put(nums[left],map.get(nums[left])-1);
-                if(map.get(nums[left])==0) 
-                map.remove(nums[left]);
-                left++;
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int low = 0;
+        int high = 0;
+        int bi = 0;
+        int move = 0;
+        int total = 0;
+        while(high < n){
+            map.put(nums[high], map.getOrDefault(nums[high], 0)+1);
+            while(map.size() > k){
+                if(map.containsKey(nums[low])){
+                    map.put(nums[low], map.get(nums[low])-1);
+                    if(map.get(nums[low]) == 0) map.remove(nums[low]);
+                }
+                low++;
+                bi = low;
             }
-            ans += right - left + 1;
-            right++;
-        }
-        return ans;
+            while(map.get(nums[low]) > 1)
+                map.put(nums[low], map.get(nums[low++])-1);
+            
+            if(map.size() == k) total = total + (low - bi + 1);
 
+            high ++;
+        }
+        return total;
     }
 }
