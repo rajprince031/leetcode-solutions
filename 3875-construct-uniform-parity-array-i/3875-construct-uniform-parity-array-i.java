3945b303865e41cc8e43e1cb1e60b6eb91169f60
Prince Raj <rajprince031@gmail.com>
2026-09-03 22:25:59 +0530

    Time: 0 ms (100%), Space: 45.2 MB (58.38%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniformArray(int[] nums) {
+        // boolean isOdd = false;
+        // int min = nums[0];
+        // for(int n : nums){
+        //     min = Math.min(n, min);
+        //     if((n&1) == 1){
+        //         isOdd = true;
+        //     }
+        // }
+        // if((min&1)==1) return true;
+        // return !isOdd;
+        return true;
+    }
+}
